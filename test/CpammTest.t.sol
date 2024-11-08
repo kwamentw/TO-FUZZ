e9cbb27ffe8afa445744c5ca607b86a2d9cff43c
kwamentw <antwi1496@hotmail.com>
2024-11-07 22:49:39 -0400

    +test README

--- test/CpammTest.t.sol
+++ test/CpammTest.t.sol
@@ -23,6 +23,9 @@ contract  CPAMMTest is Test {
 
     uint256 private mainnetFork;
 
+    /**
+     * Test setup
+     */
     function setUp() public {
         cpamm = new CPAMM(DAI,WETH);
 
@@ -41,9 +44,11 @@ contract  CPAMMTest is Test {
      * Add liquidity Helper
      */
     function addLiquidityC() public {
+        // depositing weth and approving cpamm
         weth.deposit{value: 100e18}();
         weth.approve(address(cpamm),100e18);
 
+        // depositing DAI and approving cpamm to spend
         deal(DAI, address(this), 100e18,true);
         dai.approve(address(cpamm),100e18);
 
@@ -54,36 +59,45 @@ contract  CPAMMTest is Test {
      * testing Add liquidity to cpamm
      */
     function testAddLiquidityC() public {
+        // depositing weth and approving cpamm
         weth.deposit{value: 20e18}();
         weth.approve(address(cpamm),20e18);
 
+        // depositing DAI and approving cpamm to spend
         deal(DAI, address(this), 20e18, true);
         dai.approve(address(cpamm), 20e18);
         
         uint256 sharesAdded = cpamm.addLiquidity(20e18,20e18);
 
         assertGt(sharesAdded,0);
+        console2.log("Liquidity added: ",sharesAdded);
     }
 
     //Testing swap in CPAMM
     function testSwapCPAMM() public{
+        //add liquidity
         addLiquidityC();
 
+        // Deposit weth and approve cpamm
         weth.deposit{value: 30e18}();
         weth.approve(address(cpamm),30e18);
 
+        // deposit DAI and approve cpamm
         deal(DAI,address(this), 30e18);
         dai.approve(address(cpamm), 30e18);
 
         uint256 amountOut = cpamm.swap(WETH,19e18);
         assertGt(amountOut,0);
+        console2.log("Amount out is : ", amountOut);
         
     }
 
     // Testing remove liquidity
     function testRemoveLiquidityC() public {
+        // making sure there is liquidity to remove
         addLiquidityC();
 
+        // removing liquidity
         (uint256 amount0, uint256 amount1)=cpamm.removeLiquidity(65e18);
 
         assertEq(amount0, 65e18);
