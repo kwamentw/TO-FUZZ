60c5d9c2b5caafad65004cafd41e00302a408560
kwamentw <antwi1496@hotmail.com>
2024-11-10 23:16:28 -0400

    +TEST

---
+++ test/staking.t.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {console2} from "forge-std/console2.sol";
+import {StakingRewards} from  "../src/StRewards.sol";
+
+contract StakingTest is Test{
+
+}
