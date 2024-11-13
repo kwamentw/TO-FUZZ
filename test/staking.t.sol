f754ba9dfd257adbcceac92d1a89fa5b0310e7d5
kwamentw <antwi1496@hotmail.com>
2024-11-12 20:04:28 -0400

    +TEST

--- test/staking.t.sol
+++ test/staking.t.sol
@@ -25,9 +25,31 @@ contract StakingTest is Test{
         assertEq(vm.activeFork(),mainnetFork);
     }
 
+    function stakeOwner(address topG) public {
+        deal(DAI,topG,5000e19,true);
+        vm.startPrank(topG);
+        IERC20(DAI).approve(address(staking), 5000e18);
+
+        staking.setRewardsDuration(block.timestamp + 1 days);
+
+        staking.stake(5000e18);
+        vm.stopPrank();
+    }
+
+        function stake(address topG) public {
+        deal(DAI,topG,5000e19,true);
+        vm.startPrank(topG);
+        IERC20(DAI).approve(address(staking), 5000e18);
+
+        staking.stake(5000e18);
+        vm.stopPrank();
+    }
+
     function testStake() public {
         deal(DAI,address(this),5000e19,true);
         IERC20(DAI).approve(address(staking), 5000e18);
+
+        staking.setRewardsDuration(block.timestamp + 1 days);
 
         staking.stake(5000e18);
 
@@ -35,6 +57,36 @@ contract StakingTest is Test{
 
         console2.log("You have staked: ",stakeBal);
         assertEq(stakeBal,5000e18);
+    }
+
+    function testWithdraw() public {
+        stake(address(this));
+
+        staking.withdraw(4500e18);
+
+        uint256 stakeBal = staking.balanceOf(address(this));
+        uint256 totalSupply = staking.totalSupply();
+        assertEq(stakeBal,500e18);
+        assertEq(totalSupply,500e18);
+        console2.log("Stake amount left: ", stakeBal);
+        console2.log("Total supply: ", totalSupply);
+    }
+
+    function testRewards() public{
+        vm.warp(2 days);
+        stakeOwner(address(this));
+        stake(address(0xabc));
+        stake(address(4345));
+
+        deal(USDC,address(staking),50e23);
+
+        staking.notifyRewardAmount(500e18);
+         vm.warp(block.timestamp + 8 days);
+        uint256 reward = staking.rewardPerToken();
+
+        console2.log(staking.rewardRate());
+        assertGt(reward,0);
+
     }
 
 }
