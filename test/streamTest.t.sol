3d9c8b6f9973475187298ab8d711839c339781b7
kwamentw <antwi1496@hotmail.com>
2024-12-26 19:10:00 -0400

    +Testt

--- test/streamTest.t.sol
+++ test/streamTest.t.sol
@@ -11,6 +11,11 @@ contract StreamTest is Test{
         stream = new Streaming();
     }
 
+    function createStreamm() internal returns(uint256){
+        address receiver = address(0xabc);
+        uint256 streamidd = stream.createStream{value: 20e6}(receiver, 20e6, 10 days, address(0));
+    }
+
     function testCreateStreamm() public {
         uint256 streamidd = stream.createStream{value: 20e6}(address(0xabc), 20e6, 10 days, address(0));
         address receiver = stream.getStreamInfo(streamidd).receiver;
@@ -22,5 +27,18 @@ contract StreamTest is Test{
         vm.expectRevert();
         // uint256 streamidd = stream.createStream{value: 0}(address(0xabc), 20e6, 10 days, address(0));        
         uint256 streamidd = stream.createStream{value: 20e7}(address(0xabc), 20e6, 10 days, address(0));
+    }
+
+    function testExtendStream() public {
+        uint256 id = createStreamm();
+        uint256 newStopTime = 20 days;
+        
+        uint256 oldRate = stream.getStreamInfo(id).ratePerSecond;
+        stream.extendStream(id, newStopTime);   
+        uint256 currentStopTime = stream.getStreamInfo(id).stopTime;
+        uint256 newRate = stream.getStreamInfo(id).ratePerSecond;
+
+        assertEq(currentStopTime, newStopTime);
+        assertLt(newRate,oldRate);
     }
 }
